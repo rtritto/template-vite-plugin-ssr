4104fa1edbd147cbad4ab28d1c69ca6c35c93d72
rtritto <r.tritto5@studenti.uniba.it>
2023-04-29 18:54:27 +0200

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { join } from 'node:path'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default defineConfig({
-  cacheDir: join(process.cwd(), '.cache/.vite'),
+  cacheDir: '.cache/.vite',
   plugins: [
     solid({ ssr: true }),
     ssr()
-  ],
-  build: {
-    // @ts-ignore
-    polyfillDynamicImport: false
-  }
+  ]
 })
